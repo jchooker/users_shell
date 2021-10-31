128f56dbdadd48637691f8fa788368afc764ddc9
Joseph Hooker <jcolehooker@gmail.com>
2021-10-30 23:13:59 -0500

    finished models and testing

--- users_shell_app/models.py
+++ users_shell_app/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email_address = models.EmailField(max_length=255)
+    email_address = models.EmailField(max_length=254)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}, {self.age} years old, {self.email_address}"
